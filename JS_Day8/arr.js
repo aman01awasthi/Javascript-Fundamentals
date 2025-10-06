12619dfbf5ddfd242210d151e6831fa45a6b8b1f
parvinder89 <parvinder.entritt@gmail.com>
2025-10-06 15:51:01 +0530

    Printed First, Last Element of array. And also Printed length of an Array

---
+++ JS_Day8/arr.js
@@ -0,0 +1,14 @@
+// Create an array of 5 numbers -> print first, last, and length
+let arr = [1, 2, 3, 4, 5];
+
+console.log(arr[0]);
+console.log(arr[arr.length - 1]);
+console.log(arr.length);
+
+let firstElement = arr[0];
+let lastElement = arr[arr.length - 1];
+let arrayLength = arr.length;
+
+console.log(firstElement);
+console.log(lastElement);
+console.log(arrayLength);
