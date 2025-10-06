eb0f26d5a823485ecf83cb3d21c59ec7ac185531
Amandeep Awasthi <aman01awasthi@gmail.com>
2025-10-06 23:10:54 +0530

    Task 2 of Day 8 Completed: Array Sorting, Array reversing

---
+++ JS_Day8/Task2.js
@@ -0,0 +1,9 @@
+// Create an array of your 5 favorite fruits.
+let fruits = ["Apple", "Pineapple", "Orange", "Banana", "Kiwi"];
+fruits[2] = "Mango";
+
+fruits.sort( );
+
+fruits.reverse( )
+
+console.log(fruits);
