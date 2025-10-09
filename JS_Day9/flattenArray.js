cb0880aa7852dc9ed63bfdef6251d59cd14c2b2a
Amandeep Awasthi <aman01awasthi@gmail.com>
2025-10-10 00:07:27 +0530

    Add Flatten Array exercise

---
+++ JS_Day9/flattenArray.js
@@ -0,0 +1,14 @@
+// Flatten a 2D array → 1D array
+let array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+let arr = [];
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array[i].length; j++) {
+    arr.push(array[i][j]);
+  }
+}
+console.log(arr)
