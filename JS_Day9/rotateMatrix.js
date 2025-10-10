79e18a9dabe9af0156292888018151bddc51c7ae
Amandeep Awasthi <aman01awasthi@gmail.com>
2025-10-10 09:10:24 +0530

    Implemented Day 9 JavaScript exercises: 2D array manipulations, matrix operations, and array utilities

---
+++ JS_Day9/rotateMatrix.js
@@ -0,0 +1,26 @@
+// Rotate a 2D Array (Matrix) by 90° clockwise
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+let newMatrix = [];
+for (let i = 0; i < matrix.length; i++) {
+    let arr = [];
+    for (let j = matrix[i].length - 1; j >= 0; j--) {
+        arr.push(matrix[j][i]);
+    }
+    newMatrix.push(arr);
+}
+console.log(newMatrix);
+
+// let newMatrix1 = newMatrix;
+// for (let a = matrix.length -1; a >= 0; i--) {
+//   let arr1 = [];
+//   for (let j = matrix[i].length - 1; j >= 0; j--) {
+//     arr1.push(matrix[j][i]);
+//   }
+//   newMatrix1.push(arr1);
+// }
+// console.log(newMatrix1);
