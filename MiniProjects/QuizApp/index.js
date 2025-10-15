fd63c9917683f79821ca875fb6d3fb61b659bf29
Amandeep Awasthi <aman01awasthi@gmail.com>
2025-10-15 22:49:02 +0530

    Implement Quiz App basic functionality with dynamic questions and answer validation

--- MiniProjects/QuizApp/index.js
+++ MiniProjects/QuizApp/index.js
@@ -5,18 +5,44 @@ let questions = [
     correct: "Language",
   },
 ];
+
 let currentQuestion = 0;
 function quizApp() {
   let question = questions[currentQuestion];
   let options = question.options;
+  let correct = question.correct;
   document.querySelector(".questionContainer").innerText = question.question;
-  document.querySelector(".optionArea").innerText = options;
-  console.log(options);
+  let optionArea = document.querySelector(".optionArea");
+  optionArea.innerHTML = "";
+  let selectedVal;
   for (let i = 0; i < options.length; i++) {
-        options.addEventListener("click", function () {
-        options[i] = options;
-      options.classList.add("selected");
+    let button = document.createElement("button");
+    button.textContent = options[i];
+    button.classList.add("btn", "btn-outline-primary");
+    optionArea.appendChild(button);
+    button.classList.remove("selected");
+    button.addEventListener("click", function () {
+      const allBtn = document.querySelectorAll(".optionArea button");
+      for (let button of allBtn) {
+        button.classList.remove("selected");
+      }
+      this.classList.add("selected");
+      selectedVal = this.textContent;
     });
+  }
+
+  let submitBtn = document.getElementById("submitBtn");
+  submitBtn.addEventListener("click", function () {
+    let scoreArea = document.querySelector(".scoreArea");
+    scoreArea.classList.remove("true", "false");
+    if (selectedVal == correct) {
+      scoreArea.innerText = selectedVal + " Correct Answer!!!  Got 5 points";
+      scoreArea.classList.add("true");
+    } else {
+      scoreArea.innerText = selectedVal + " Wrong Answer!!!";
+      scoreArea.classList.add("false");
     }
+  });
 }
+
 quizApp();
