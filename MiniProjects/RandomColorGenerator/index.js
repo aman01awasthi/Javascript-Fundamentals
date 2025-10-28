e937e227e389b7c6849736cc5d2a99b774fedd3e
Amandeep Awasthi <aman01awasthi@gmail.com>
2025-10-28 23:58:36 +0530

    Completed Random Color Generation and user can copy the hex color by clicking in the color box

--- MiniProjects/RandomColorGenerator/index.js
+++ MiniProjects/RandomColorGenerator/index.js
@@ -1,11 +1,15 @@
 let genBtn = document.getElementById("generateBtn");
 let displayBox = document.getElementById("colorBox");
-genBtn.addEventListener("click", function(){
-let R = Math.floor(Math.random() * 256);
-let G = Math.floor(Math.random() * 256);
-let B = Math.floor(Math.random() * 256);
+genBtn.addEventListener("click", function () {
+  let R = Math.floor(Math.random() * 256);
+  let G = Math.floor(Math.random() * 256);
+  let B = Math.floor(Math.random() * 256);
+  let hexColor =
+    R.toString(16).padStart(2, "0") +
+    G.toString(16).padStart(2, "0") +
+    B.toString(16).padStart(2, "0");
 
-console.log(R+G+B)
-    console.log("clicked")
-    displayBox.style.color = "#0000"; 
-})
+let finalColor = "#" + hexColor;
+console.log(finalColor);
+  displayBox.style.backgroundColor = finalColor;
+});
